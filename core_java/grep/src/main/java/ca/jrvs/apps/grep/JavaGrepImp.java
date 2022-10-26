802b0954fa10749dc0741ed2173361795446542c
Raymond Van <vraymond028@gmail.com>
2022-10-26 19:39:27 +0000

    Implemented java grep

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,125 @@
+package ca.jrvs.apps.grep;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JavaGrepImp implements JavaGrep {
+
+  final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
+  private String regex;
+  private String rootPath;
+  private String outFile;
+  private Pattern pattern;
+
+  public static void main(String[] args) throws IOException {
+    if (args.length != 3) {
+      throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
+    }
+
+    JavaGrepImp javaGrepImp = new JavaGrepImp();
+    javaGrepImp.setRegex(args[0]);    // eg.: .*Romeo.*Juliet.*"
+    javaGrepImp.setRootPath(args[1]); // eg.: grep/data/txt
+    javaGrepImp.setOutFile(args[2]);  // eg.: grep/out/outputFile.txt
+
+    try {
+      javaGrepImp.process();
+    } catch (Exception ex) {
+      javaGrepImp.logger.error("Error: Unable to process", ex);
+    }
+  }
+
+  @Override
+  public void process() throws IOException {
+    List<String> matchedLines = new ArrayList<>();
+    List<File> files = listFiles(this.rootPath);
+    List<String> lines = readLines(files.get(0));
+    for (File file : listFiles(this.rootPath)) {
+      for (String line : readLines(file)) {
+        if (containsPattern(line)) {
+          matchedLines.add(line);
+        }
+      }
+    }
+    writeToFile(matchedLines);
+  }
+
+  @Override
+  public List<File> listFiles(String rootDir) {
+    File f = new File(rootDir);
+    return Arrays.asList(f.listFiles());
+  }
+
+  @Override
+  public List<String> readLines(File inputFile) throws IllegalArgumentException {
+    List<String> lines = new ArrayList<>();
+    try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        lines.add(line);
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return lines;
+  }
+
+  @Override
+  public boolean containsPattern(String line) {
+    Matcher matcher = pattern.matcher(line);
+    return matcher.find();
+  }
+
+  @Override
+  public void writeToFile(List<String> lines) throws IOException {
+    try {
+      FileWriter writer = new FileWriter(this.outFile);
+      for(String line : lines) {
+        writer.write(line + System.lineSeparator());
+      }
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public String getRootPath() {
+    return rootPath;
+  }
+
+  @Override
+  public void setRootPath(String rootPath) {
+    this.rootPath = rootPath;
+  }
+
+  @Override
+  public String getRegex() {
+    return regex;
+  }
+
+  @Override
+  public void setRegex(String regex) {
+    this.regex = regex;
+    this.pattern = Pattern.compile(regex);
+  }
+
+  @Override
+  public String getOutFile() {
+    return outFile;
+  }
+
+  @Override
+  public void setOutFile(String outFile) {
+    this.outFile = outFile;
+  }
+}
